c03d36702cd39d71c7f1c06e3fef5b553d26b0b3
Moritz Lottmann <moritz.lottmann@gmail.com>
2020-11-19 10:01:39 +0100

    Ableitung mit Beispiel

--- U_1/CP1_U1_diffquot_skeleton.py
+++ U_1/CP1_U1_diffquot_skeleton.py
@@ -1,44 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 def diffquot(fhandle, a, b, h):
-    """ Berechne den rechtseitigen Differenzenquotienten.
 
-    Argumente
-    ---------
-    fhandle : function
-        die zu differenzierende Funktion
-    a : int
-        unter Intervallgrenze
-    b : int
-        obere Intervallgrenze
-    h : float
-        Schrittweite der Differentiation
+xwerte = np.arange(a, b+h, h)                 #X-Werte von a bis b+h mit h "Abstand"
 
-    Output: 2-Tupel
-    ------
-    xwerte : float
-        Definitionsbereich der Funktion (1-d array)
-    ableitung : float
-        Ergebnisvektor mit der numerischen Ableitung (1-d array)
-
-    Funktionsaufruf
-    ---------------
-        xwerte, ableitung = diffquot(fhandle, a, b, h)
-
-    Beispiel
-    --------
-        xwerte, ableitung = diffquot(np.sin, -10, 10, 0.1)
-    """
-
-    xwerte = np.arange(a,b+h,h)                 #X-Werte von a bis b+h mit h "Abstand"
     ywerte = fhandle(xwerte)                    #Funktionswerte zu den X-Werten
     ableitung = []                              #leeres array für die Ableitungswerte
-    for y1,y2 in zip(ywerte[:-1],ywerte[1:]):   #funktionswerte an der stelle i und i+1 also f(xi) und f(xi+h) da xi+1 = xi+h
+
+    for y1, y2 in zip(ywerte[:-1], ywerte[1:]):   #funktionswerte an der stelle i und i+1 also f(xi) und f(xi+h) da xi+1 = xi+h
         value = (y2-y1)/h                       #Ableitungswert berechnet mit f(xi+h)-f(xi)/h
         ableitung.append(value)                 #anhängen an das array
 
     ableitung = np.array(ableitung)             #umwandlung in np array (np arrays lassen sich besser verrechnen)
     return (xwerte[:-1], ableitung)             #return der x werte und der ableitung
+
 
 '''Test mit Sinus'''
 xval, diff = diffquot(np.sin,-10,10,0.1)
