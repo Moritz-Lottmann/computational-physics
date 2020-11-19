e67f0752229d6eeec37f2e065a33915ba8a25514
Moritz Lottmann <moritz.lottmann@gmail.com>
2020-11-19 10:00:55 +0100

    Ableitung mit beispiel

--- U_1/CP1_U1_diffquot_skeleton.py
+++ U_1/CP1_U1_diffquot_skeleton.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 def diffquot(fhandle, a, b, h):
     """ Berechne den rechtseitigen Differenzenquotienten.
 
@@ -28,14 +30,17 @@ def diffquot(fhandle, a, b, h):
         xwerte, ableitung = diffquot(np.sin, -10, 10, 0.1)
     """
 
-    import numpy as np
+    xwerte = np.arange(a,b+h,h)                 #X-Werte von a bis b+h mit h "Abstand"
+    ywerte = fhandle(xwerte)                    #Funktionswerte zu den X-Werten
+    ableitung = []                              #leeres array für die Ableitungswerte
+    for y1,y2 in zip(ywerte[:-1],ywerte[1:]):   #funktionswerte an der stelle i und i+1 also f(xi) und f(xi+h) da xi+1 = xi+h
+        value = (y2-y1)/h                       #Ableitungswert berechnet mit f(xi+h)-f(xi)/h
+        ableitung.append(value)                 #anhängen an das array
 
-    # Bestimmung des Definitionsbereichs der Funktion
+    ableitung = np.array(ableitung)             #umwandlung in np array (np arrays lassen sich besser verrechnen)
+    return (xwerte[:-1], ableitung)             #return der x werte und der ableitung
 
-    # Evaluierung der Funktion 'fhandle'
-
-
-    # Berechung der numerischen Ableitung mittels des rechtsseitigen
-    # Differenzenquotienten
-
-    return (xwerte[:-1], ableitung)
+'''Test mit Sinus'''
+xval, diff = diffquot(np.sin,-10,10,0.1)
+plt.plot(xval, np.sin(xval), xval, diff)
+plt.show()
